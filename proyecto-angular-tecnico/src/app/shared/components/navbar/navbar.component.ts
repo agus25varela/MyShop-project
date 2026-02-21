90fa75e511beb436a9453e41c088da253677a259
Agustina Varela <agusvarela2002@hotmail.com>
2026-02-21 16:32:34 -0300

    feat: menu desplegable en el apartado del usuario al haber iniciado sesion

--- proyecto-angular-tecnico/src/app/shared/components/navbar/navbar.component.ts
+++ proyecto-angular-tecnico/src/app/shared/components/navbar/navbar.component.ts
@@ -14,14 +14,30 @@ export class NavbarComponent {
   public authService = inject(AuthService); // Lo hacemos público para usarlo en el HTML
   private router = inject(Router);
   isMenuOpen = false;
+  isUserMenuOpen = false;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  closeUserMenu(): void {
+    this.isUserMenuOpen = false;
+  }
+
+  navigateToCart(): void {
+    this.router.navigate(['/cart']);
+    this.closeUserMenu();
+    this.isMenuOpen = false;
+  }
+
   logout() {
     this.authService.logout();
     this.isMenuOpen = false;
+    this.closeUserMenu();
     this.router.navigate(['/auth/login']);
   }
 }
